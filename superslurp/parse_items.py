01509ad5868f5fd9c1c19918807472c1259a1bd3
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2025-02-22 22:06:58 +0100

    Handle parsing of multiple objects of the same type bought and reduction

--- superslurp/parse_items.py
+++ superslurp/parse_items.py
@@ -32,6 +32,7 @@ class Item(TypedDict):
 def parse_items(text: str) -> list[Item]:
     current_category: Category = Category.UNDEFINED
     items: list[Item] = []
+    previous_line = None
     for line in text.split("\n"):
         if not line.strip():
             continue
@@ -40,38 +41,88 @@ def parse_items(text: str) -> list[Item]:
             # print(f"Changed category from {current_category} to {new_category.value}")
             current_category = new_category
             continue
-        items_info = get_items_infos_from_line(line)
-        # print(items_info)
-        name, price, tr, quantity = "", "0,00 €", "", 1
-        if len(items_info) > 4:
-            # Sometimes there's two spaces by mistake in a name
-            *temp_name, tr, price, _ = items_info
-            name = " ".join(temp_name)
-        elif len(items_info) == 4:
-            name, tr, price, _ = items_info
-        elif len(items_info) == 3:
-            name, price, _ = items_info
+        if "COUPON N°" in line:
+            # This is a reduction we don't care about
+            break
+        if previous_line is not None:
+            if "Pourcentage: 30" in previous_line:
+                # This is a reduction on the previous item
+                items[-1]["price"] = items[-1]["price"] * 0.7
+                print(
+                    f"Reducing the price of {items[-1]['name']} by 30%, new item: {items[-1]}"
+                )
+                previous_line = None
+                continue
+            new_line = f"{previous_line}{line}"
+            print(f"Couldn't handle {previous_line!r}, trying with:\n{new_line}")
+            items_info = get_items_infos_from_line(new_line)
+            # print("Items info:", items_info)
+            if (
+                item := get_item_from_item_infos_multiple(current_category, items_info)
+            ) is None:
+                print(f"Couldn't handle {new_line}, skipping.")
+                continue
+            previous_line = None
         else:
-            # print(f"Couldn't handle {items_info}, skipping line.")
-            continue
-        final_name, grams = _get_gram(name)
-        item: Item = {
-            "category": current_category.value,
-            "name": final_name,
-            "price": _get_price(price),
-            "quantity": quantity,
-            "grams": grams,
-            "tr": _get_tr(tr),
-        }
-        # print(f"New item : {item}")
+            items_info = get_items_infos_from_line(line)
+            if len(items_info) < 3:
+                print(f"Can't handle {items_info} alone, merging with next line.")
+                previous_line = line
+                continue
+            item = get_item_from_item_infos(current_category, items_info)
+        print(f"New item : {item}")
         items.append(item)
     return items
+
+
+def get_item_from_item_infos_multiple(
+    current_category: Category, items_info: list[str]
+) -> Item | None:
+    if len(items_info) != 6:
+        # Can't deal with this line
+        return None
+    name, tr, quantity, unit_price, *_ = items_info
+    quantity = quantity.split(" x")[0]
+    return {
+        "category": current_category.value,
+        "name": name,
+        "price": _get_price(unit_price),
+        "quantity": int(quantity),
+        "grams": _get_gram(name)[1],
+        "tr": _get_tr(tr),
+    }
+
+
+def get_item_from_item_infos(current_category: Category, items_info: list[str]) -> Item:
+    name, price, tr, quantity = "", "0,00 €", "", 1
+    if len(items_info) > 4:
+        # Sometimes there's two spaces by mistake in a name
+        # But this could be something else, it's suspicious
+        print(f"Too many elements in {items_info}")
+        *temp_name, tr, price, _ = items_info
+        name = " ".join(temp_name)
+    elif len(items_info) == 4:
+        # Item that can be paid with TR
+        name, tr, price, _ = items_info
+    elif len(items_info) == 3:
+        # An item that can't be paid with TR
+        name, price, _ = items_info
+    final_name, grams = _get_gram(name)
+    item: Item = {
+        "category": current_category.value,
+        "name": final_name,
+        "price": _get_price(price),
+        "quantity": quantity,
+        "grams": grams,
+        "tr": _get_tr(tr),
+    }
+    return item
 
 
 def _get_gram(name: str) -> tuple[str, float | None]:
     grams = None
     search = re.search(
-        r"(?P<multiplier>\d+X)?(?P<grams>\d?[\d+\,]?\d+K?G(?: ENVIRON)?)", name
+        r"(?P<multiplier>\d+X)?(?P<grams>\d?[\d+,]?\d+K?G(?: ENVIRON)?)", name
     )
     if search is None:
         return name, None

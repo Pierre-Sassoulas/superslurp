46a1d2f868830d16d539916b71db8ae17cf42692
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2025-02-22 23:48:04 +0100

    Better test of all the parsed elements

--- tests/test_parse_text.py
+++ tests/test_parse_text.py
@@ -5,7 +5,7 @@ import json
 import pytest
 
 # pylint: disable-next=import-private-name
-from superslurp.parse_items import Category, _get_gram
+from superslurp.parse_items import Category, Item, _get_gram
 from superslurp.parser import parse_text
 
 
@@ -20,9 +20,9 @@ def expected_parsed_items() -> dict[Category, int]:
     with open(
         "tests/fixtures/sample_expected_parsed_items.json", encoding="utf8"
     ) as file:
-        expected_len = json.load(file)
+        expected = json.load(file)
     result = {}
-    for key, value in expected_len.items():
+    for key, value in expected.items():
         result[Category(key)] = value
     return result
 
@@ -46,7 +46,7 @@ def test_quantity(name: str, expected: tuple[str, int | None]) -> None:
 
 
 def test_parse_items(
-    sample_txt: str, expected_parsed_items: dict[Category, int]
+    sample_txt: str, expected_parsed_items: dict[Category, list[Item]]
 ) -> None:
     receipt = parse_text(sample_txt)
 
@@ -55,11 +55,24 @@ def test_parse_items(
 
     items_by_categories = receipt["items"]
     assert isinstance(items_by_categories, dict)
-    for category in Category:
-        assert len(items_by_categories[category]) == expected_parsed_items[category]
     for items in items_by_categories.values():
         for item in items:
             assert item["name"] is not None
             assert item["price"] is not None
             assert item["quantity"] is not None or item["grams"] is not None
             assert item["tr"] is not None
+    for category in Category:
+        assert len(items_by_categories[category]) == len(
+            expected_parsed_items[category]
+        )
+        for i, item_and_expected_item in enumerate(
+            zip(
+                items_by_categories[category],
+                expected_parsed_items[category],
+                strict=True,
+            )
+        ):
+            item, expected_item = item_and_expected_item
+            assert item == expected_item, (
+                f"Item {i} in category {category} is different"
+            )

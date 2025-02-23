dfcb36fef3330321ccd1db4e0015db83b24fbe75
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2025-02-23 16:52:29 +0100

    Add some caching to not convert to pdf all the time during debug

--- superslurp/__main__.py
+++ superslurp/__main__.py
@@ -28,8 +28,17 @@ def make_json_serializable(
 
 
 def parse_superu_receipt(filename: str | Path) -> str:
-    logging.debug("Converting pdf receipt to text...")
-    text = extract_text(filename)
+    path = Path(filename)
+    cached_text_file = Path(path.parent / f".{path.name}.txt")
+    if not cached_text_file.exists():
+        logging.debug(f"Converting {path!r} to text...")
+        text = extract_text(filename)
+        with open(cached_text_file, "w", encoding="utf8") as f:
+            f.write(text)
+    else:
+        logging.debug("Reading text from cache without converting from pdf...")
+        with open(cached_text_file, encoding="utf8") as f:
+            text = f.read()
     logging.debug("Extracted text, parsing receipt...")
     receipt = parse_text(text)
     logging.debug("Parsing done, checking consistency...")

78d927b15235c0987f317f7a0bacf0c467df1afd
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2025-02-26 22:14:45 +0100

    [check] Use math.isclose, in comparison

--- superslurp/check/consistency.py
+++ superslurp/check/consistency.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import math
+
 from superslurp.check.calculate_total_from_items import _calculate_totals_from_items
 from superslurp.exception import UnexpectedSumOfParsedItems
 from superslurp.superslurp_typing import Receipt
@@ -12,7 +14,8 @@ def check_consistency(receipt: Receipt) -> None:
         recalculated_total_discount,
         recalculated_eligible_tr,
     ) = _calculate_totals_from_items(receipt)
-    if recalculated_total != receipt["total"]:
+
+    if not math.isclose(recalculated_total, receipt["total"]):
         raise UnexpectedSumOfParsedItems(
             receipt,
             "total",
@@ -20,9 +23,12 @@ def check_consistency(receipt: Receipt) -> None:
             recalculated_total,
             receipt["total"],
         )
-    if receipt["subtotal"] != 0.0:
-        if receipt["total_discount"] != 0.0:
-            if recalculated_total != receipt["subtotal"] + receipt["total_discount"]:
+
+    if not math.isclose(receipt["subtotal"], 0.0):
+        if not math.isclose(receipt["total_discount"], 0.0):
+            if not math.isclose(
+                recalculated_total, receipt["subtotal"] + receipt["total_discount"]
+            ):
                 raise UnexpectedSumOfParsedItems(
                     receipt,
                     "total_discount",
@@ -30,7 +36,7 @@ def check_consistency(receipt: Receipt) -> None:
                     recalculated_total,
                     receipt["subtotal"] + receipt["total_discount"],
                 )
-        if recalculated_sub_total != receipt["subtotal"]:
+        if not math.isclose(recalculated_sub_total, receipt["subtotal"]):
             raise UnexpectedSumOfParsedItems(
                 receipt,
                 "subtotal",
@@ -39,9 +45,9 @@ def check_consistency(receipt: Receipt) -> None:
                 receipt["subtotal"],
             )
 
-    if (
-        receipt["total_discount"] != 0.0
-        and recalculated_total_discount != receipt["total_discount"]
+    if not math.isclose(receipt["total_discount"], 0.0) and not math.isclose(
+        recalculated_total_discount,
+        receipt["total_discount"],
     ):
         raise UnexpectedSumOfParsedItems(
             receipt,
@@ -50,9 +56,8 @@ def check_consistency(receipt: Receipt) -> None:
             recalculated_total_discount,
             receipt["total_discount"],
         )
-    if (
-        receipt["eligible_tr"] != 0.0
-        and recalculated_eligible_tr != receipt["eligible_tr"]
+    if not math.isclose(receipt["eligible_tr"], 0.0) and not math.isclose(
+        recalculated_eligible_tr, receipt["eligible_tr"]
     ):
         raise UnexpectedSumOfParsedItems(
             "eligible_tr",

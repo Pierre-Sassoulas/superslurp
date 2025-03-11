b63110861bb8294a8f99abfadab511cd7076e753
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2025-03-11 07:09:14 +0100

    Handle '.' instead of expected ',' in prices

--- superslurp/parse/parse_items.py
+++ superslurp/parse/parse_items.py
@@ -19,8 +19,8 @@ single_undefined_category_pattern = re.compile(
 # way_of_paying_pattern = re.compile(r"(?P<way_of_paying>\d{2} \n)+")
 items_pattern = re.compile(
     r"(?P<name>[\w .\/%,=€°+Éé\*]*)(?P<tr>\(T\))?(\d\d)?[ \n]*"
-    r"(?P<quantity>[\d x,]+ €)?[ ]+(?P<price>[\d]+,[ \d€]+)[ ]?(?P<way_of_paying>\d{2})+ \n"
-    r"|\s*Pourcentage:\s*(?P<pourcentage>\d+)\s*-(?P<discount>[\d]+,|.[ \d€]+)\n"
+    r"(?P<quantity>[\d x,]+ €)?[ ]+(?P<price>[\d]+[,|\.][ \d€]+)[ ]?(?P<way_of_paying>\d{2})+ \n"
+    r"|\s*Pourcentage:\s*(?P<pourcentage>\d+)\s*-(?P<discount>[\d]+[,|\.][ \d€]+)\n"
 )
 
 
